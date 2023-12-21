a4a681897355a355c6284e139dac6b3eb9fac5cd
Kundan Bhosale <62865281+kundanbhosale@users.noreply.github.com>
2023-12-22 00:18:46 +0530

    fix

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig, configDefaults } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import path from "path"
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
-    globalSetup: "src/__tests__/setup/env.ts",
+    globalSetup: "__tests__/setup/env.ts",
     environment: "jsdom",
-    exclude: [...configDefaults.exclude, "src/__tests__/e2e/*"],
+    exclude: [...configDefaults.exclude, "__tests__/e2e/*"],
   },
 })
